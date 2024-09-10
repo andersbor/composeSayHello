43194ad7d0e84d95a2dcc2f4274082a19ebe37f3
andersbor <anbo@zealand.dk>
2024-09-10 08:53:47 +0200

    gradle update

--- app/src/main/java/com/example/sayhello/MainActivity.kt
+++ app/src/main/java/com/example/sayhello/MainActivity.kt
@@ -9,11 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,8 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.sayhello.ui.theme.SayHelloTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,10 +54,11 @@ fun SayHello(modifier: Modifier = Modifier) {
             OutlinedTextField(
                 value = name,
                 onValueChange = { name = it },
-                label = { Text("Enter your name") }
+                label = { Text("Enter your name") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
             )
-            Button(
-                onClick = { message = name.ifEmpty { "nobody" } }) {
+            Button(onClick = { message = name.ifEmpty { "nobody" } })
+            {
                 Text("Say Hello")
             }
         }
